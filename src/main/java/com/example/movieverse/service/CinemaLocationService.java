f5ecc3b9f344c92a2e6f21fcbcd72205cd7f6852
kevinDronnn <voroshilin777@icloud.com>
2024-08-20 15:46:10 +0300

    added CinemaLocation controller, service and repository

---
+++ src/main/java/com/example/movieverse/service/CinemaLocationService.java
@@ -0,0 +1,22 @@
+package com.example.movieverse.service;
+
+import com.example.movieverse.dto.CinemaLocationDto;
+import com.example.movieverse.mapper.CinemaLocationMapper;
+import com.example.movieverse.repository.CinemaLocationRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CinemaLocationService {
+
+    private final CinemaLocationRepository cinemaLocationRepository;
+
+    public CinemaLocationService(CinemaLocationRepository cinemaLocationRepository) {
+        this.cinemaLocationRepository = cinemaLocationRepository;
+    }
+
+    public List<CinemaLocationDto> getAllCinemaLocations() {
+        return CinemaLocationMapper.INSTANCE.CinemaLocationToCinemaLocationDto(cinemaLocationRepository.findAll());
+    }
+}
