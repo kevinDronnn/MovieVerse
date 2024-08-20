1a70b18ebd73d07e228705d2441d78a0ab1aa547
kevinDronnn <voroshilin777@icloud.com>
2024-08-20 14:08:29 +0300

    Initial commit

---
+++ src/test/java/com/example/movieverse/MovieVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movieverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieVerseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
